7bfcbdf7909fc6d46af99ebff215f87222e41ad5
Romain Bacque <bacqueromain@orange.fr>
2023-02-21 09:56:30 +0100

    style: global style improvement

---
+++ client/src/config/index.js
@@ -0,0 +1,4 @@
+export default config = {
+  host: "http://localhost:4000/",
+  apiKey: "5149dcf4bdb945948a3cc9e1ce24589c",
+};
---
+++ client/src/pages/GitesPage.tsx
@@ -0,0 +1,8 @@
+import Gites from "../components/Layout/Gites";
+
+// component
+const GitesPage: React.FC = () => {
+  return <Gites />;
+};
+
+export default GitesPage;
