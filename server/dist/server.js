e5d4863b63a7969d740f8e665d0dccb699f63a4f
Romain Bacque <bacqueromain@orange.fr>
2025-09-29 11:03:48 +0200

    fix: various bug fixes

---
+++ server/dist/server.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// import debug from "debug";
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = require("dotenv");
+const _1 = __importDefault(require("."));
+(0, dotenv_1.config)(); // Load environment variables from .env file
+const app = (0, express_1.default)();
+const port = process.env.PORT ?? 3000;
+app.use(_1.default);
+app.listen(port, () => console.log(`listening on port ${port}`));
