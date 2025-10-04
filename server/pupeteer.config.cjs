8cab6a2e1a4aaea0d34c402ebe6ef28f54c9666a
Romain Bacque <bacqueromain@orange.fr>
2025-10-04 07:48:31 +0200

    fix: various bug fixes

--- server/pupeteer.config.cjs
+++ server/pupeteer.config.cjs
@@ -4,6 +4,7 @@ const { join } = require("path");
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  // Changes the cache location for Puppeteer.
+  // Changes the cache location for Puppeteer. it's used to store the Chromium browser binary.
+  // By default, it's stored in the user's home directory, but you can change it to a project-specific directory.
   cacheDirectory: join(__dirname, ".cache", "puppeteer"),
 };
