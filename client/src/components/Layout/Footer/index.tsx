c9d0cab331403be4fb371fcc12a093efbc1f86c1
Romain Bacque <bacqueromain@orange.fr>
2025-10-21 09:19:36 +0200

    fix: bad seo

--- client/src/components/Layout/Footer/index.tsx
+++ client/src/components/Layout/Footer/index.tsx
@@ -4,12 +4,12 @@ import classes from "./style.module.css";
 const Footer: React.FC = () => {
   return (
     <footer className={classes.footer}>
-      <p>© 2022 <span className="bold">Gites ariège.</span> Tous droits réservés.</p>
+      <p>
+        © 2022 <span className="bold">Gites ariège.</span> Tous droits réservés.
+      </p>
       <div>
         <span>Contact : </span>
-        <a href="mailto:bacqueflorence@wanadoo.fr">
-          bacqueflorence@wanadoo.fr
-        </a>
+        <a href="mailto:bacqueflorence@wanadoo.fr">jf.gite.09@orange.fr </a>
       </div>
     </footer>
   );
