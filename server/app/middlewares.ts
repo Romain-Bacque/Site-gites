75ac8f9ca6cb69204a218c64e4f219ac2d9ef8b0
Romain Bacque <bacqueromain@orange.fr>
2025-10-23 10:47:48 +0200

    fix: bad seo

--- server/app/middlewares.ts
+++ server/app/middlewares.ts
@@ -6,7 +6,7 @@ export const csrfProtection = csurf({
   cookie: {
     httpOnly: false,
     secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    sameSite: "none",
   },
 });
 
@@ -33,7 +33,7 @@ export const createCSRFToken = (req: Request, res: Response): void => {
   res.cookie("XSRF-TOKEN", req.csrfToken(), {
     secure: process.env.NODE_ENV === "production",
     httpOnly: false,
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    sameSite: "none",
   });
 
   res.status(200).json({ csrfToken });
