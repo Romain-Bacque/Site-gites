f4140e0e43342c6f0d874d621c4de8cdf5a1ff8e
Romain Bacque <bacqueromain@orange.fr>
2026-01-23 15:38:49 +0100

    feat: convert to next.js v16

--- server/app/controllers/authController.ts
+++ server/app/controllers/authController.ts
@@ -10,7 +10,7 @@ import { resendEmailHandler } from "../utilities/emailhandler";
 const debug = debugLib("controller:auth");
 
 const redirectFn = (isValid: boolean) => {
-  return `${process.env.CORS_ORIGIN}/email-confirm?isValid=${isValid}`;
+  return `${process.env.CORS_ORIGIN}/auth/email-confirm?isValid=${isValid}`;
 };
 
 const getCookieConfig = (): CookieOptions => ({
@@ -149,7 +149,7 @@ const authController = {
     const user = new User({ username, password, email });
 
     const accessToken = generateAccessToken({ id: user.id });
-    const link = `${process.env.CORS_ORIGIN}/email-confirm?id=${user.id}&token=${accessToken}`;
+    const link = `${process.env.HOST}/email-confirm?id=${user.id}&token=${accessToken}`;
 
     await sendEmail({
       service: "gmail",
